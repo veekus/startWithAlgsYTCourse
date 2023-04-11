fe0fa32b381200383c5d2e13a49cc9d1146313c3
vimi0721 <viktoriia.mirzoian@netcracker.com>
2023-04-11 13:53:29 +0300

    Task 263

--- src/Task263.java
+++ src/Task263.java
@@ -4,12 +4,18 @@ import java.util.*;
 class Task263 {
 
     void solve(Scanner in, PrintWriter out){
-        int a = in.nextInt();
-        int a2 = in.nextInt();
-        int a3 = in.nextInt();
-        int max = Math.max(a2, a3);
-        int min = Math.min(a2, a3);
-        out.println(Math.min(max-min-1, a-max+min-1));
+        int k1 = in.nextInt();
+        int l1 = in.nextInt();
+        int m1 = in.nextInt();
+        int k2 = in.nextInt();
+        int l2 = in.nextInt();
+        int m2 = in.nextInt();
+
+        int currentBolts = k1-k1*l1/100;
+        int currentScrews = k2-k2*l2/100;
+        int change = (currentBolts - currentScrews) > 0 ? (currentBolts - currentScrews)*m1 : Math.abs(currentBolts - currentScrews)*m2;
+        int res = k1*l1*m1/100 + k2*l2*m2/100 + change;
+        out.println(res);
     }
 
     void run(){
