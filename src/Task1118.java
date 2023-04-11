a9a74c7b45771ca0d567e64fdbc84ff39bb1465b
vimi0721 <viktoriia.mirzoian@netcracker.com>
2023-04-11 17:32:48 +0300

    Task 1118 updated

--- src/Task1118.java
+++ src/Task1118.java
@@ -7,13 +7,8 @@ class Task1118 {
         int h = in.nextInt();
         int a = in.nextInt();
         int b = in.nextInt();
-        int s = 0;
-        int days = 0;
-        while (s+a<h){
-            s = s+a-b;
-            days++;
-        }
-        out.println(days+1);
+        int res = ((h-a) + (a-b) -1 ) / (a-b) +1;
+        out.println(Math.max(res, 1));
     }
 
     void run(){
